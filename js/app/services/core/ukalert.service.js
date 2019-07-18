cc5f9b81c865610761970d776a962ef07d21fcce
Guilherme Rocha <guilherme@candiba.net>
2019-07-18 20:38:03 -0300

    console outputs removed

--- js/app/services/core/ukalert.service.js
+++ js/app/services/core/ukalert.service.js
@@ -8,7 +8,7 @@ angular.module('ukalert', [])
           var prepMsg = "<ul class='uk-list'>";
           for (let key in msg) {
               prepMsg = prepMsg.concat('<li>' + msg[key] + '<li>');
-              console.log(key + ' - ' + msg[key]);
+              
           }
           prepMsg = prepMsg + "<ul>";   
           UIkit.notification(prepMsg,{status: level}).then(function () {
@@ -17,7 +17,7 @@ angular.module('ukalert', [])
             }
           });
         } else {
-          console.log(msg);
+          
           UIkit.notification(msg,{status: level}).then(function () {
             if(action) {
               window.location = action;
